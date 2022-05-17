eb24f6fb7fea5ff12714222977ab2af64ebab2c9
urmil22 <urmilbhavsar8@gmail.com>
2022-05-17 11:49:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const url = 'https://reqres.in/api/users?page=1'
+  const [users, setUsers] = useState([]);
+
+  
+  
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const userData = await response.json();
+    setUsers(userData.data);
+    console.log(userData);
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello Everyone 👋</h1>
+    <button onClick={fetchData} >Fetch Users</button>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+             <article key={user.id}>
+               
+            <img  src={user.avatar} alt={user.first_name} />
+               <h2>{user.first_name} {user.last_name}</h2>
+               <h4>email: {user.email}</h4>
+             </article>
+
+          )
+          )}
+        </ul>
+      )}
     </div>
   );
 }
